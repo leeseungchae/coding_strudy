085a446777257facc69545c3a65f4822437c1d55
user <ztmdco@gmial.com>
2023-01-14 22:43:55 +0900

    [Feat] longestCommonPrefix

--- Logest Common prefix.py
+++ Logest Common prefix.py
@@ -1,4 +1,15 @@
-def longestCommonPrefix(strs):
+from typing import List
+
+
+def longestCommonPrefix(strs: List[str]) -> str:
+    """Longest Common Prefix
+    Write a function to find the longest common prefix string amongst an array of strings.
+    If there is no common prefix, return an empty string "".
+
+    :argument strs: list
+    :return str
+    """
+
     check = None
     if len(strs) == 1:
         check = strs[0]
@@ -8,7 +19,7 @@ def longestCommonPrefix(strs):
             if idx == 1:
                 for i in range(len(strs) - idx):
                     word_2 = strs[idx + i]
-                    hap = ''
+                    hap = ""
                     for i, j in zip(word, word_2):
                         if i == j:
                             hap = hap + i
@@ -26,11 +37,11 @@ def longestCommonPrefix(strs):
         return check
 
 
-"""Longest Common Prefix
-Write a function to find the longest common prefix string amongst an array of strings.
-If there is no common prefix, return an empty string "". """
+""" input spec:
+    영어 단어들로 이루어진 리스트만 입력 가능하고,
+    단어의 공백 및 알파벳 이외의 글자는 허용하지 않는다."""
 
-Input = ["flower","flow","flight"]
+Input = ["flower", "flow", "flight"]
 result = longestCommonPrefix(Input)
 
 print(result)
